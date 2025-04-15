4906cf474421d07cdb76b889f0ba73afa740f529
sasasaqwerterdfg <dhrdhmat@gmail.com>
2025-04-15 13:32:04 +0330

    Update scripts.js

--- myfolder/scripts.js
+++ myfolder/scripts.js
@@ -1,8 +1,8 @@
 const images = [
   { src: "images/2.png", title: "سایه‌ی جادویی", link: "page1.html" },
   { src: "images/1.png", title: "سیاه‌چاله آبی", link: "page2.html" },
-  { src: "images/3.png", title: "موجود مرموز", link: "page3.html" }
-  { src: "images/4.png", title: "سیاه چاه", link: "page4.html" }
+  { src: "images/3.png", title: "موجود مرموز", link: "page3.html" },
+  { src: "images/4.png", title: "سیاه چاه", link: "page4.html" },
   { src: "images/5.png", title: "فرو پاشی", link: "page5.html" }
 ];
 
